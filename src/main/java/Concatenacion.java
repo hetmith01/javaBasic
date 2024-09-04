af7c26a9255d7c80198f546f98c5054919383ccc
hetmith01 <het.hanc.14@gmail.com>
2024-09-04 15:28:14 -0500

    variables y concatenación

---
+++ src/main/java/Concatenacion.java
@@ -0,0 +1,13 @@
+public class Concatenacion {
+    public static void main(String[] args) {
+        String nombre = "Hetmith";
+        String apellido = "Hancco";
+
+        String palabra1 = "mi", palabra2 = "github";
+
+        System.out.println(nombre);
+        System.out.println(apellido);
+        System.out.println(nombre + " " + apellido);
+        System.out.println(palabra1 + " " + palabra2);
+    }
+}
---
+++ src/main/java/variables.java
@@ -0,0 +1,8 @@
+public class variables {
+    public static void main(String[] args) {
+        String nombre = "hht";
+        int valor = 100;
+        double valor2 = 10.5;
+        System.out.println(nombre);
+    }
+}
