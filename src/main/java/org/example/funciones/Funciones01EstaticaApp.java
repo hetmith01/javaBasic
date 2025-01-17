83fa5e781900e6e69a524f74aceb6ad16f8132b2
Hetmith <het.hanc.14@gmail.com>
2025-01-17 03:49:04 -0500

    implementacion de paquetes funciones01

---
+++ src/main/java/org/example/funciones/Funciones01EstaticaApp.java
@@ -0,0 +1,15 @@
+package org.example.funciones;
+
+//import static org.example.funciones.*;
+import static org.example.funciones.Funciones01Estaticas.*;
+
+public class Funciones01EstaticaApp {
+    public static void main(String[] args) {
+        saludar();
+
+        int res;
+
+        res = mostrar();
+        System.out.println("el valor de res es: "+ res);
+    }
+}
---
+++ src/main/java/org/example/funciones/Funciones01Estaticas.java
@@ -0,0 +1,14 @@
+package org.example.funciones;
+
+public class Funciones01Estaticas {
+
+
+    public static void saludar(){
+        System.out.println("HOla a todos mis amigos");
+    }
+
+    //la palabra static, implica poder usar la clase sin instanciar un objeto de la clase
+    public static int mostrar(){
+        return 10;
+    }
+}
